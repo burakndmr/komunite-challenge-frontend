51d44a748c91699559aba882c0e8c9d673411151
Burak Demir <buraknasifdemir123@gmail.com>
2023-03-06 13:35:40 +0300

    add todos molecule

---
+++ src/components/molecules/Todos.tsx
@@ -0,0 +1,12 @@
+// Components
+import TodoItem from "../molecules/TodoItem";
+// Types
+type Props = {};
+
+export default function Todos({}: Props) {
+  return (
+    <div className="">
+      <TodoItem />
+    </div>
+  );
+}
